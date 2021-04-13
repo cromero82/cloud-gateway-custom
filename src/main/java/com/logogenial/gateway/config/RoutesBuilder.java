3ed6083b34e0dc99420a0b2cb16d861c3159c460
Carlos Andres Romero <git.alianza.com.co>
2021-04-12 21:04:26 -0500

    agrega routeLocator builder basico

--- src/main/java/com/logogenial/gateway/config/RoutesBuilder.java
+++ src/main/java/com/logogenial/gateway/config/RoutesBuilder.java
@@ -1,4 +1,16 @@
 package com.logogenial.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RoutesBuilder {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes().route( r -> r.path("/api/**")
+                .uri("http://localhost:8090/")
+            ).build();
+    }
 }
