85b1241dce1ea12f269ed428cd86edf7608a23f6
Carlos Andres Romero <git.alianza.com.co>
2021-04-12 23:04:18 -0500

    agrega filtro personalizado para autenticacion y se omite para solicitar token

--- src/main/java/com/logogenial/gateway/config/RoutesBuilder.java
+++ src/main/java/com/logogenial/gateway/config/RoutesBuilder.java
@@ -1,5 +1,7 @@
 package com.logogenial.gateway.config;
 
+import com.logogenial.gateway.filters.CustomTokenAutenticatedFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +9,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RoutesBuilder {
+
+    @Autowired
+    private CustomTokenAutenticatedFilter filterFactory;
+
+
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder){
-        return builder.routes().route( r -> r.path("/api/**")
-                .uri("http://localhost:8090/")
-            ).build();
+        return builder.routes()
+                .route("login", r -> r.path("/api/**")
+                        .uri("http://localhost:8090/"))
+                .route("alls", r -> r.path("/v1/**")
+                        .filters(f ->
+                                f.filters(filterFactory.apply(new CustomTokenAutenticatedFilter.Config()))
+                                .removeRequestHeader("Cookie"))
+                        .uri("http://localhost:8090/"))
+                .build();
     }
 }
